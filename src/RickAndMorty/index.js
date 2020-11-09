a39d007aafa44b41d9f78e56029a8d3c842a8c6a
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2020-11-09 13:32:12 -0500

    Actualizacion: Ya se esta viendo todos los characters y parte de sus descripciones, falta la dimension

--- src/RickAndMorty/index.js
+++ src/RickAndMorty/index.js
@@ -11,59 +11,45 @@ const fetchData = async(url_api) => {
     .catch(err => console.error(err))
 }
 
-const renderElement = (info) => {
+const renderElement = async(data) => {
     const app = document.getElementById('app')
     console.log(app)
     const characterView = document.getElementById('character-view')
     console.log(characterView)
-    //console.log('this is the data:' , data)
-    characterView.innerHTML =
-    // const html = data.map(character =>
-    // )    
-    //app.innerHTML = html.join('');
-    
-    `<div id="container">
-        <ul id="character-info">
-            <div class="characterPic">
-                <img src="${info.pic}" alt="" />
-            </div>
-            <li>
-                ${info.count}
-            </li>
-            <li>
-                ${info.name}
-            </li>
-            <li>
-                ${info.dimension}
-            </li>
-        </ul>
-    </div>`
-    
-
-    
-    app.innerHTML = characterView.innerHTML 
+  
+    // const character = await fetchData(API)
+    console.log(data.results)
+    const html = data.results.map(x => 
+                `<div id="container">
+                <ul id="character-info">
+                    <div class="characterPic">
+                        <img src="${x.image}" alt="" />
+                    </div>
+                    <li>
+                        ${x.gender}
+                    </li>
+                    <li>
+                        ${x.name}
+                    </li>
+                    <li>
+                        ${x.dimension}
+                    </li>
+                </ul>
+            </div>`   
+    )
+    app.innerHTML = html.join('')
     
 }
+
+            
+
 
 
 const renderInfo = async() => {
     
     try {
-        
         const data = await fetchData(API)
-        const results = data.results
-        const characterId = await fetchData(`${API}${data.results[0].id}`)
-        const origin = await fetchData(characterId.origin.url)
-        
-        const info = {
-            count:data.info.count,
-            name: characterId.name,
-            pic: characterId.image,
-            dimension:origin.dimension,
-        }  
-        const why = results.map(x => renderElement(info))
-       
-                    
+        renderElement(data)             
     }catch (error){
         console.error(error)
     }
